9220670798c46f1996b2a86905e8bc06dfd214e6
Zhumazhenis Dairabay <zhumazhenis@gmail.com>
2019-11-03 17:25:23 +0600

    project init

---
+++ kumys/src/main/java/group/orda/kumys/KumysApplication.java
@@ -0,0 +1,13 @@
+package group.orda.kumys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumysApplication.class, args);
+	}
+
+}
---
+++ kumys/src/test/java/group/orda/kumys/KumysApplicationTests.java
@@ -0,0 +1,13 @@
+package group.orda.kumys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KumysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
